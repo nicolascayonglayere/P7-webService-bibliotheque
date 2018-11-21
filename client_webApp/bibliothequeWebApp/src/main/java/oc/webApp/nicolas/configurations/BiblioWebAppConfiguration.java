9f13fb7d80493a9ecc632e3dfbb881b452f8fd5d
nicolas cayon-glayere <nicolas.cayon.glayere@gmail.com>
2018-11-21 14:21:20 +0100

    fichiers dockerfile et nettoyage dépendances

--- client_webApp/bibliothequeWebApp/src/main/java/oc/webApp/nicolas/configurations/BiblioWebAppConfiguration.java
+++ client_webApp/bibliothequeWebApp/src/main/java/oc/webApp/nicolas/configurations/BiblioWebAppConfiguration.java
@@ -14,13 +14,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 import fr.yogj.bibliows.BiblioWS;
 
 @Configuration
 @ConfigurationProperties(prefix = "cxf")
-public class BiblioWebAppConfiguration { // {extends JaxRsProxyClientConfiguration {
+public class BiblioWebAppConfiguration {
 
 	/**
 	 * Méthode pour obtenir le SpringBus
@@ -36,99 +35,15 @@ public class BiblioWebAppConfiguration { // {extends JaxRsProxyClientConfigurati
 	@Value("${biblio.ws.endpoint-url}")
 	private String url;
 
-	@Value("${biblio.ws.key-store}")
-	private Resource keyStore;
-
-	@Value("${biblio.ws.key-store-password}")
-	private String keyStorePassword;
-
-	@Value("${biblio.ws.trust-store}")
-	private Resource trustStore;
-
-	@Value("${biblio.ws.trust-store-password}")
-	private String trustStorePassword;
-
 	@Bean
 	public BiblioWS bus(SpringBus bus) {
 		JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
 		bean.setServiceClass(BiblioWS.class);
 		bean.setAddress(this.url);
 		bean.setBus(bus);
-
-		// KeyStore ks;
-		// try {
-		// ks = KeyStore.getInstance("JKS");
-		//
-		// ks.load(this.keyStore.getInputStream(), this.keyStorePassword.toCharArray());
-		//
-		// logger.info("Loaded keystore: " + this.keyStore.getURI().toString());
-		// try {
-		// this.keyStore.getInputStream().close();
-		// } catch (IOException e) {
-		// }
-		// KeyManagerFactory keyManagerFactory = KeyManagerFactory
-		// .getInstance(KeyManagerFactory.getDefaultAlgorithm());
-		// keyManagerFactory.init(ks, this.keyStorePassword.toCharArray());
-		//
-		// KeyStore ts = KeyStore.getInstance("JKS");
-		// ts.load(this.trustStore.getInputStream(),
-		// this.trustStorePassword.toCharArray());
-		// logger.info("Loaded trustStore: " + this.trustStore.getURI().toString());
-		// try {
-		// this.trustStore.getInputStream().close();
-		// } catch (IOException e) {
-		// }
-		// TrustManagerFactory trustManagerFactory = TrustManagerFactory
-		// .getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		// trustManagerFactory.init(ts);
-		//
-		// HttpsUrlConnectionMessageSender messageSender = new
-		// HttpsUrlConnectionMessageSender();
-		// messageSender.setKeyManagers(keyManagerFactory.getKeyManagers());
-		// messageSender.setTrustManagers(trustManagerFactory.getTrustManagers());
-		//
-		// // otherwise: java.security.cert.CertificateException: No name matching
-		// // localhost found
-		// messageSender.setHostnameVerifier((hostname, sslSession) -> {
-		// if (hostname.equals("localhost")) {
-		// return true;
-		// }
-		// return false;
-		// });
-		//
-		// bean.setConduitSelector((ConduitSelector) messageSender);
-		// // bean.setFeatures(Collections.singletonList(new LoggingFeature()));
-		//
-		// } catch (KeyStoreException e1) {
-		// // TODO Auto-generated catch block
-		// e1.printStackTrace();
-		// } catch (NoSuchAlgorithmException e1) {
-		// // TODO Auto-generated catch block
-		// e1.printStackTrace();
-		// } catch (CertificateException e1) {
-		// // TODO Auto-generated catch block
-		// e1.printStackTrace();
-		// } catch (IOException e1) {
-		// // TODO Auto-generated catch block
-		// e1.printStackTrace();
-		// } catch (UnrecoverableKeyException e1) {
-		// // TODO Auto-generated catch block
-		// e1.printStackTrace();
-		// }
 		return bean.create(BiblioWS.class);
 	}
 
-	// @Bean
-	// public WebServerFactoryCustomizer<UndertowServletWebServerFactory>
-	// containerCustomizer() {
-	// return (WebServerFactoryCustomizer) factory -> {
-	// UndertowServletWebServerFactory undertowFactory =
-	// (UndertowServletWebServerFactory) factory;
-	// undertowFactory.getBuilderCustomizers().add(builder -> {
-	// builder.addHttpListener(httpPort, httpInterface);
-	// });
-	// };
-	// }
 	@Bean
 	public FilterRegistrationBean<Filter> someFilterRegistration() {
 		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<Filter>();
--- deploiement_docker/webApp/generated-sources/cxf/fr/yogj/bibliows/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.yogj.fr/biblioWS/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fr.yogj.bibliows.types;
